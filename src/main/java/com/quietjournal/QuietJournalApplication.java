c2bab7b424505ba6ca3cf94589539ebaaaa8f8bc
sidraRashdi <rashdimuntaha557@gmail.com>
2025-09-07 02:19:11 +0500

    chore: initial Spring Boot project setup with PostgreSQL config

---
+++ src/main/java/com/quietjournal/QuietJournalApplication.java
@@ -0,0 +1,13 @@
+package com.quietjournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuietJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuietJournalApplication.class, args);
+	}
+
+}
